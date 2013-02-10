eb932ae39453b2de1d71a95e5392dcce1e6fc50d
Jonathan Chu <jonathan.chu@me.com>
2013-02-10 00:09:02 -0500

    Add django-storages and boto to requirements.  Modify production settings to use S3 for static assets.

--- stashdaddy/settings/prod.py
+++ stashdaddy/settings/prod.py
@@ -1,3 +1,18 @@
+import os
+import S3
 from .base import *
 
-DEBUG = True
+# AWS credentials
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+
+# S3 storage
+INSTALLED_APPS.append('storages')
+STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_CALLING_FORMAT = S3.CallingFormat.SUBDOMAIN
+AWS_AUTO_CREATE_BUCKET = True
+AWS_QUERYSTRING_AUTH = False
+AWS_EXPIREY = 60 * 60 * 24 * 7
+AWS_HEADERS = {'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIREY, AWS_EXPIREY)}
+STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
--- stashdaddy/wsgi.py
+++ stashdaddy/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stashdaddy.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stashdaddy.settings.dev")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
