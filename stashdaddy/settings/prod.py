38ce36773149d181c13d5b411647ef281178c38b
Jonathan Chu <jonathan.chu@me.com>
2013-02-10 02:11:45 -0500

    Prod settings fail again.

--- stashdaddy/settings/prod.py
+++ stashdaddy/settings/prod.py
@@ -4,7 +4,7 @@ from .base import *
 
 # AWS credentials
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
 # S3 storage
