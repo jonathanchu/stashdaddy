7c668d290d2aff0f8bfebb98ce502bcb617c4842
Jonathan Chu <jonathan.chu@me.com>
2013-02-13 02:04:21 -0500

    Refer to new CachedS3StaticStorage for compress.

--- stashdaddy/settings/prod.py
+++ stashdaddy/settings/prod.py
@@ -1,22 +1,6 @@
 # import S3
 from .base import *
 
-from django.core.files.storage import get_storage_class
-from storages.backends.s3boto import S3BotoStorage
-
-class CachedS3BotoStorage(S3BotoStorage):
-    """
-    S3 storage backend that saves the files locally, too.
-    """
-    def __init__(self, *args, **kwargs):
-        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
-        self.local_storage = get_storage_class(
-            "compressor.storage.CompressorFileStorage")()
-
-    def save(self, name, content):
-        name = super(CachedS3BotoStorage, self).save(name, content)
-        self.local_storage._save(name, content)
-        return name
 
 # AWS credentials
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
@@ -26,10 +10,12 @@ AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 # S3 storage
 INSTALLED_APPS.append('storages')
 # STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = COMPRESS_STORAGE = CachedS3BotoStorage
+STATICFILES_STORAGE = DEFAULT_FILE_STORAGE = COMPRESS_STORAGE = 'storage.CachedS3StaticStorage'
 # AWS_CALLING_FORMAT = S3.CallingFormat.SUBDOMAIN
 AWS_AUTO_CREATE_BUCKET = True
 AWS_QUERYSTRING_AUTH = False
 AWS_EXPIREY = 60 * 60 * 24 * 7
 AWS_HEADERS = {'Cache-Control': 'max-age=%d, s-maxage=%d, must-revalidate' % (AWS_EXPIREY, AWS_EXPIREY)}
 STATIC_URL = 'https://s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
+
+DEBUG = True
