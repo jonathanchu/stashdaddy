3d2d8426c8d621d0005592d7bab21eb3ca7c8fe9
Jonathan Chu <jonathan.chu@me.com>
2013-04-04 22:01:09 -0400

    Remove debug is true in production settings.

--- stashdaddy/settings/prod.py
+++ stashdaddy/settings/prod.py
@@ -1,8 +1,6 @@
 from .base import *
 
 
-DEBUG = TEMPLATE_DEBUG = True
-
 # AWS credentials
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
