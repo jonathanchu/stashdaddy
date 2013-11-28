301a4d1dd66e47939209a824ba86eb4a80d55c97
Jonathan Chu <jonathan.chu@me.com>
2013-11-28 13:48:12 -0500

    Initial commit of bookmarks app.

---
+++ bookmarks/api.py
@@ -0,0 +1,9 @@
+# from tastypie.resources import ModelResource
+
+from .models import Bookmark
+
+
+# class BookmarkResource(ModelResource):
+#     class Meta:
+#         queryset = Bookmark.objects.all()
+#         resource_name = 'bookmark_resource'
