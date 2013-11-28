ac697fc284b35bf97ef3df7946cd12788e342970
Jonathan Chu <jonathan.chu@me.com>
2013-11-28 13:54:32 -0500

    Add registration to local apps in settings.

--- stashdaddy/settings/base.py
+++ stashdaddy/settings/base.py
@@ -1,132 +1,236 @@
+"""Common settings and globals."""
+
 import os
 import sys
 
-from postgresify import postgresify
 from unipath import Path
 
 
-# BASIC SETUP
-PROJECT_ROOT = Path(__file__).ancestor(2)
-sys.path.insert(0, PROJECT_ROOT)
-DEBUG = TEMPLATE_DEBUG = False
-ROOT_URLCONF = 'stashdaddy.urls'
-SECRET_KEY = os.environ['SECRET_KEY']
-SITE_ID = 1
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+########## PATH CONFIGURATION
+# Absolute filesystem path to the Django project directory:
+PROJECT_ROOT = Path(__file__).ancestor(3)
+# DJANGO_ROOT = dirname(dirname(abspath(__file__)))
+
+# Absolute filesystem path to the top-level project folder:
+# SITE_ROOT = dirname(DJANGO_ROOT)
+SITE_ROOT = os.path.dirname(PROJECT_ROOT)
+
+# Site name:
+# SITE_NAME = basename(DJANGO_ROOT)
+SITE_NAME = os.path.basename(PROJECT_ROOT)
+
+# Add our project to our pythonpath, this way we don't need to type our project
+# name in our dotted import paths:
+# path.append(DJANGO_ROOT)
+sys.path.append(PROJECT_ROOT)
+########## END PATH CONFIGURATION
+
+
+########## DEBUG CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = False
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
+TEMPLATE_DEBUG = DEBUG
+########## END DEBUG CONFIGURATION
+
+
+########## MANAGER CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = (
-    ('Jonathan Chu', 'jc@3atmospheres.com'),
+    ('Your Name', 'your_email@example.com'),
 )
 
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#managers
+MANAGERS = ADMINS
+########## END MANAGER CONFIGURATION
 
-# DATABASES
-DATABASES = postgresify()
 
-# MEDIA, ASSETS, ETC.
+########## GENERAL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#time-zone
+TIME_ZONE = 'America/New_York'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#language-code
+LANGUAGE_CODE = 'en-us'
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#site-id
+SITE_ID = 1
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
+USE_I18N = True
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
+USE_L10N = True
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#use-tz
+USE_TZ = True
+########## END GENERAL CONFIGURATION
+
+
+########## MEDIA CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-root
+# MEDIA_ROOT = normpath(join(SITE_ROOT, 'media'))
 MEDIA_ROOT = PROJECT_ROOT.child('media')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#media-url
 MEDIA_URL = '/media/'
+########## END MEDIA CONFIGURATION
+
+
+########## STATIC FILE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+# STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
 STATIC_ROOT = PROJECT_ROOT.child('static')
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [PROJECT_ROOT.child('assets')]
+
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
+STATICFILES_DIRS = (
+    PROJECT_ROOT.child('assets'),
+)
+
+# See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 )
+########## END STATIC FILE CONFIGURATION
 
-# TEMPLATES
-TEMPLATE_DIRS = [PROJECT_ROOT.child('templates')]
 
-# MIDDLEWARE
-MIDDLEWARE_CLASSES = [
+########## SECRET CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+# Note: This key only used for development and testing.
+SECRET_KEY = os.environ['SECRET_KEY']
+########## END SECRET CONFIGURATION
+
+
+########## FIXTURE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#std:setting-FIXTURE_DIRS
+# FIXTURE_DIRS = (
+#     normpath(join(SITE_ROOT, 'fixtures')),
+# )
+########## END FIXTURE CONFIGURATION
+
+
+########## TEMPLATE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-context-processors
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+)
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-loaders
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-dirs
+TEMPLATE_DIRS = (
+    PROJECT_ROOT.child('templates'),
+)
+########## END TEMPLATE CONFIGURATION
+
+
+########## MIDDLEWARE CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#middleware-classes
+MIDDLEWARE_CLASSES = (
+    # Default Django middleware.
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-]
+    # Uncomment the next line for simple clickjacking protection:
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+########## END MIDDLEWARE CONFIGURATION
 
-# TEMPLATE CONTEXT PROCESSORS
-TEMPLATE_CONTEXT_PROCESSORS = [
-    "django.contrib.auth.context_processors.auth",
-    "django.core.context_processors.request",
-    "django.core.context_processors.debug",
-    "django.core.context_processors.i18n",
-    "django.core.context_processors.media",
-    # "django.core.context_processors.static",
-]
 
-# APPS
-INSTALLED_APPS = [
-    # django-suit needs to be before django.contrib.admin
-    'suit',
+########## URL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#root-urlconf
+ROOT_URLCONF = 'stashdaddy.urls'
+########## END URL CONFIGURATION
 
-    # django
+
+########## APP CONFIGURATION
+DJANGO_APPS = (
+    # Default Django apps:
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    # third-party packages
-    'compressor',
-    # 'djcelery',
-    'django_forms_bootstrap',
-    'gunicorn',
-    # 'kombu.transport.django',
-    'raven.contrib.django.raven_compat',
+    # Useful template tags:
+    'django.contrib.humanize',
+)
+
+THIRD_PARTY_APPS = (
     'south',
-    'taggit',
-    # 'tastypie',
+    'compressor',
+    'registration',
+)
 
-    # stashdaddy apps
-    # 'accounts',
-    'bookmarks',
-    'core',
-]
+# Apps specific for this project go here.
+LOCAL_APPS = (
+)
 
-# LOGGING
-#
-# Logging
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
+########## END APP CONFIGURATION
+
+
+########## LOGGING CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#logging
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
     'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
     },
     'loggers': {
         'django.request': {
-            'handlers': ['console'],
+            'handlers': ['mail_admins'],
             'level': 'ERROR',
             'propagate': True,
         },
     }
 }
+########## END LOGGING CONFIGURATION
 
-# AUTH STUFF
-ACCOUNT_ACTIVATION_DAYS = 7
-LOGIN_REDIRECT_URL = '/'
-# AUTH_USER_MODEL = 'accounts.CustomUser'
-# AUTH_PROFILE_MODULE = 'accounts.UserProfile'
-TWITTER_CONSUMER_KEY = ''
-TWITTER_CONSUMER_SECRET = ''
-POCKET_API_KEY = os.environ['POCKET_API_KEY']
 
-# ANALYTICS
-GAUGES_SITE_ID = os.environ['GAUGES_SITE_ID']
+########## WSGI CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#wsgi-application
+WSGI_APPLICATION = 'stashdaddy.wsgi.application'
+########## END WSGI CONFIGURATION
 
-# TASK QUEUE
-BROKER_BACKEND = 'django'
 
-# COMPRESSOR SETTINGS
+########## COMPRESSOR CONFIGURATION
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 COMPRESS_ROOT = PROJECT_ROOT.child('assets')
@@ -139,38 +243,9 @@ COMPRESS_CSS_FILTERS = [
 COMPRESS_JS_FILTERS = [
     'compressor.filters.jsmin.JSMinFilter'
 ]
+########## END COMPRESSOR CONFIGURATION
 
-# DJANGO-SUIT CONFIG
-SUIT_CONFIG = {
-    # header
-    'ADMIN_NAME': 'Stashdaddy Admin',
-    # 'HEADER_DATE_FORMAT': 'l, j. F Y',
-    # 'HEADER_TIME_FORMAT': 'H:i',
 
-    # forms
-    # 'SHOW_REQUIRED_ASTERISK': True,  # Default True
-    # 'CONFIRM_UNSAVED_CHANGES': True, # Default True
-
-    # Parameter also accepts url name
-    # 'SEARCH_URL': 'admin:auth_user_changelist',
-    # Set to empty string if you want to hide search from menu
-    # 'SEARCH_URL': ''
-
-    # menu
-    # 'SEARCH_URL': '/admin/auth/user/',
-    # 'MENU_ICONS': {
-    #    'sites': 'icon-leaf',
-    #    'auth': 'icon-lock',
-    # },
-    # 'MENU_OPEN_FIRST_CHILD': True, # Default True
-    'MENU_EXCLUDE': ('auth.group',),
-    # 'MENU': (
-    #     'sites',
-    #     {'app': 'auth', 'icon':'icon-lock', 'models': ('user', 'group')},
-    #     {'label': 'Settings', 'icon':'icon-cog', 'models': ('auth.user', 'auth.group')},
-    #     {'label': 'Support', 'icon':'icon-question-sign', 'url': '/support/'},
-    # ),
-
-    # misc
-    # 'LIST_PER_PAGE': 15
-}
+########## ACCOUNTS CONFIGURATION
+ACCOUNT_ACTIVATION_DAYS = 7
+########## END ACCOUNTS CONFIGURATION
