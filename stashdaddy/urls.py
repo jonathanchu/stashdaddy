6fa71635b8ba3615004d7d709a54677d2d11731a
Jonathan Chu <jonathan.chu@me.com>
2013-11-28 13:47:37 -0500

    Update URLs to use django-registration and remove old apps not used.

--- stashdaddy/urls.py
+++ stashdaddy/urls.py
@@ -2,15 +2,13 @@ from django.conf.urls import include, patterns, url
 from django.views.generic import TemplateView
 
 from django.contrib import admin
-from bookmarks.views import BookmarkList, BookmarkCreate, MyBookmarks, BookmarkEdit
-from accounts.views import Login, Register
-# from bookmarks.api import BookmarkResource
+from bookmarks.views import BookmarkList, BookmarkCreate, MyBookmarks, \
+    BookmarkEdit
 
-
-# bookmark_resource = BookmarkResource()
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(r'^$',                         TemplateView.as_view(template_name='home.html'), name='home'),
     # url(r'^(?P<username>[\-\.\w]+)/$', BookmarkList.as_view(),   name='bookmarks_list'),
     url(r'^new/$',                     BookmarkCreate.as_view(), name='bookmark_create'),
@@ -19,12 +17,13 @@ urlpatterns = patterns('',
     # (r'^api/', include(bookmark_resource.urls)),
 
     # accounts
-    (r'^accounts/',                    include('accounts.urls')),
+    (r'^accounts/', include('registration.backends.default.urls')),
 
-    url(r'^register/$',                Register.as_view(),                             name='register'),
-    url(r'^thanks/$',                  TemplateView.as_view(template_name="home.html"), name='thanks'),
+    # temp convenience auth urls
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     # url(r'^login/$',                   Login.as_view(), name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+
+    # admin
     url(r'^admin/', include(admin.site.urls)),
 )
