c502c19f86d7d79edaf0fc52eb71719b315d533c
Jonathan Chu <jonathan.chu@me.com>
2013-11-28 13:51:16 -0500

    Remove old apps.
    Simply moving up directory structure so it's not tucked away.

--- stashdaddy/bookmarks/api.py
+++
@@ -1,9 +0,0 @@
-# from tastypie.resources import ModelResource
-
-from .models import Bookmark
-
-
-# class BookmarkResource(ModelResource):
-#     class Meta:
-#         queryset = Bookmark.objects.all()
-#         resource_name = 'bookmark_resource'
